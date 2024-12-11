1e22914c88282a401ed2efa262d2fe26fbd55a65
Dan Vassallo <odanvas@amazon.com>
2024-12-11 15:10:49 -0500

    borrow @joshfri keymaps and utils

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,8 @@
 --
 -- See the kickstart.nvim README for more information
 --
+--
+-- auto commands
 local function augroup(name)
   return vim.api.nvim_create_augroup('mnv_' .. name, { clear = true })
 end
@@ -129,6 +131,163 @@ vim.api.nvim_create_autocmd('User', {
     end)
   end,
 })
+
+-- Utils
+local M = {}
+
+function M.EqualizeSplits()
+  vim.cmd 'wincmd =' -- Equalize the size of all windows
+end
+
+function M.SmartDelete()
+  if vim.fn.getline '.' == '' then
+    return '"_dd'
+  end
+  return 'dd'
+end
+
+-- ty @ https://github.com/adibhanna/nvim/blob/main/lua/config/utils.lua
+function M.CopyFilePathAndLineNumber()
+  local current_file = vim.fn.expand '%:p'
+  local current_line = vim.fn.line '.'
+  local is_git_repo = vim.fn.system('git rev-parse --is-inside-work-tree'):match 'true'
+
+  if is_git_repo then
+    local current_repo = vim.fn.systemlist('git remote get-url origin')[1]
+    local current_branch = vim.fn.systemlist('git rev-parse --abbrev-ref HEAD')[1]
+
+    -- Convert Git URL to GitHub web URL format
+    current_repo = current_repo:gsub('git@github.com:', 'https://github.com/')
+    current_repo = current_repo:gsub('%.git$', '')
+
+    -- Remove leading system path to repository root
+    local repo_root = vim.fn.systemlist('git rev-parse --show-toplevel')[1]
+    if repo_root then
+      current_file = current_file:sub(#repo_root + 2)
+    end
+
+    local url = string.format('%s/blob/%s/%s#L%s', current_repo, current_branch, current_file, current_line)
+    vim.fn.setreg('+', url)
+    print('Copied to clipboard: ' .. url)
+  else
+    -- If not in a Git directory, copy the full file path
+    vim.fn.setreg('+', current_file .. '#L' .. current_line)
+    print('Copied full path to clipboard: ' .. current_file .. '#L' .. current_line)
+  end
+end
+
+-- Keymaps
+
+local keymap = vim.keymap.set
+
+keymap('n', 'zj', 'o<Esc>k', { desc = 'Create a line above without insert' })
+keymap('n', 'zk', 'O<Esc>j', { desc = 'Create a line below without insert' })
+keymap('n', '<leader>pv', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle Neo-Tree' })
+
+keymap('n', '<leader>0', function()
+  EqualizeSplits()
+end, { noremap = true, silent = true })
+
+-- Resize window using <shift> arrow keys since we have remapped cmd + h/j/k/l as arrow keys this is really convenient
+keymap('n', '<S-Up>', '<cmd>resize +2<CR>')
+keymap('n', '<S-Down>', '<cmd>resize -2<CR>')
+keymap('n', '<S-Left>', '<cmd>vertical resize -2<CR>')
+keymap('n', '<S-Right>', '<cmd>vertical resize +2<CR>')
+
+-- move lines
+keymap('n', '<A-j>', ':m .+1<CR>==')
+keymap('v', '<A-j>', ":m '>+1<CR>gv=gv")
+keymap('i', '<A-j>', '<Esc>:m .+1<CR>==gi')
+keymap('n', '<A-k>', ':m .-2<CR>==')
+keymap('v', '<A-k>', ":m '<-2<CR>gv=gv")
+keymap('i', '<A-k>', '<Esc>:m .-2<CR>==gi')
+
+-- Add undo break-points
+keymap('i', ',', ',<c-g>u')
+keymap('i', '.', '.<c-g>u')
+keymap('i', ';', ';<c-g>u')
+
+keymap('n', 'J', 'mzJ`z')
+
+-- Center screen after C-d / C-u
+keymap('n', '<C-d>', '<C-d>zz')
+keymap('n', '<C-u>', '<C-u>zz')
+
+keymap('n', 'N', 'Nzzzv')
+
+keymap('n', 'n', 'nzzzv')
+
+-- paste from register without overriding register
+keymap('x', '<leader>p', [["_dP]])
+
+-- yank to global(?) clipboard ...i.e. can now paste outside of vim buffers
+keymap({ 'n', 'v' }, '<leader>y', [["+y]])
+keymap('n', '<leader>Y', [["+Y]])
+
+-- delete without overwriting vim register
+keymap({ 'n', 'v' }, '<leader>d', [["_d]])
+
+keymap('n', 'Q', '<nop>')
+
+keymap('n', '<leader>f', vim.lsp.buf.format)
+
+-- search and replace word under cursor
+keymap('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- nice way to exit normal mode
+keymap('i', 'jj', '<esc>')
+keymap('i', 'kk', '<esc>')
+
+keymap('n', '<leader>sv', '<cmd> vsplit<CR><C-w>w', { desc = 'Split pane vertically' })
+keymap('n', '<leader>sh', '<cmd> split<CR><C-w>w', { desc = 'Split pane horizontally' })
+
+-- Diagnostic keymaps
+keymap('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+keymap('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+keymap('n', '<leader>de', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+keymap('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Stay in visual mode when changing the indent for selection
+keymap('v', '<', '<gv')
+keymap('v', '>', '>gv')
+
+-- Map enter to ciw in normal mode
+keymap('n', '<CR>', 'ciw')
+
+keymap({ 'n', 'o', 'x' }, '<s-h>', '^')
+keymap({ 'n', 'o', 'x' }, '<s-l>', 'g_')
+
+keymap('n', '<leader>L', 'vg_', { desc = 'Select to end of line' })
+keymap('n', '<leader>pa', 'ggVGp', { desc = 'Select all and paste' })
+keymap('n', '<leader>sa', 'ggVG', { desc = 'Select all' })
+keymap('n', '<leader>gp', '`[v`]', { desc = 'Select pasted text' })
+keymap('n', '<BS>', '^', { desc = 'Move to first non-blank char' })
+keymap(
+  'n',
+  '<leader>cpf',
+  ':let @+ = expand("%:p")<cr>:lua print("Copied path to: " .. vim.fn.expand("%:p"))<cr>',
+  { desc = 'Copy current file name and path', silent = false }
+)
+
+keymap('n', 'U', '<C-r>')
+keymap('n', 'U', '<C-r>')
+
+keymap('n', '<leader>xo', ':e <C-r>+<CR>', { noremap = true, desc = 'Go to location in clipboard' })
+
+keymap('n', '<leader>x', function()
+  utils.CopyFilePathAndLineNumber()
+end, { noremap = true, desc = 'copy file path and line number' })
+
+keymap('n', 'dd', function()
+  if vim.fn.getline '.' == '' then
+    return '"_dd'
+  end
+  return 'dd'
+end, { expr = true })
+
+keymap('n', '<leader>dm', ':delm! | delm A-Z0-9<CR>', {})
+
+-- Lazyvim config
 
 return {
   -- Add Dracula theme
